8e666e5fd9900c76a7c9e077c069051f52089d4f
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2022-04-05 20:34:05 +0000

    WIP

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
-	"time"
-
 	"net/url"
 	"os"
-
-	"context"
+	"regexp"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
@@ -39,50 +39,87 @@ func main() {
 
 	collector := colly.NewCollector(
 		colly.AllowedDomains(u.Host),
+		colly.Async(true),
+		colly.URLFilters(
+			regexp.MustCompile(`https://www.sudbury\.com(|/local-news.+)$`),
+			regexp.MustCompile(`https://www.sudbury\.com/membership.+`),
+			regexp.MustCompile(`https://www.sudbury\.com/weather.+`),
+		),
+		// colly.Debugger(&debug.LogDebugger{}),
 	)
+
+	// Limit the number of threads started by colly to two
+	// when visiting links which domains' matches "*sudbury.com" glob
+	collector.Limit(&colly.LimitRule{
+		DomainGlob:  "*sudbury.com",
+		Parallelism: 2,
+		Delay:       500 * time.Millisecond,
+	})
 
 	collector.OnHTML("a.section-item", func(element *colly.HTMLElement) {
 		href := element.Attr("href")
 		title := element.Text
+		src := ""
+
 		element.DOM.Find("img").Each(func(i int, s *goquery.Selection) {
 			// For each item found, get the src
-			src, foo := s.Attr("src")
-			fmt.Printf("Review %d: %s -- %t\n", i, src, foo)
+			dataSrc, found := s.Attr("data-src")
+			src = ""
+			if found {
+				src = dataSrc
+			}
 		})
 
 		article := Article{
 			Title: title,
 			Href:  href,
-			// Image: image,
+			Image: src,
 		}
 
 		articles = append(articles, article)
+	})
+
+	collector.OnHTML(`a[href]`, func(e *colly.HTMLElement) {
+		foundURL := e.Request.AbsoluteURL(e.Attr("href"))
+		// foundURL := e.Attr("href")
+		collector.Visit(foundURL)
 	})
 
 	collector.OnRequest(func(request *colly.Request) {
 		fmt.Println("Visiting", request.URL.String())
 	})
 
-	collector.Visit(webpage)
+	collector.OnScraped(func(r *colly.Response) {
+		// writeTopic(articles)
+	})
 
-	writeTopic(articles)
+	fmt.Println("Webpage:", webpage)
+	collector.Visit(webpage)
+	collector.Wait()
 }
 
-func writeTopic(data []Article) {
+func writeTopic(articles []Article) {
 	conn, err := kafka.DialLeader(context.Background(), "tcp", kafka_uri, topic, partition)
 	if err != nil {
 		log.Fatal("failed to dial leader:", err)
 	}
 
 	messages := make([]kafka.Message, 0)
-	for i := 0; i < len(data); i++ {
+	for i := 0; i < len(articles); i++ {
+		// Convert article struct to json
+		article, err := json.Marshal(articles[i])
+		if err != nil {
+			panic(err)
+		}
+
 		messages = append(messages, kafka.Message{
-			Key:   []byte(data[i].Href),
-			Value: []byte(data[i].Title),
+			Key:   []byte(articles[i].Href),
+			Value: article,
 		})
 	}
 
 	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	fmt.Println("Write")
 	_, err = conn.WriteMessages(messages...)
 	if err != nil {
 		log.Fatal("failed to write messages:", err)
