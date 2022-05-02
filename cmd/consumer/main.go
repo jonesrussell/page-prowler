5929a7e4abbb8782aec7b48a0dd279bc69438c5c
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2022-05-02 02:07:58 +0000

    feature: moved post json to file

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -23,6 +23,43 @@ type Entity struct {
 	Id   string `json:"id"`
 }
 
+type PostPhoto struct {
+	Data Data `json:"data"`
+}
+
+type Data struct {
+	Type          string        `json:"type"`
+	Attributes    Attributes    `json:"attributes"`
+	Relationships Relationships `json:"relationships"`
+}
+
+type Attributes struct {
+	FieldPost       FieldPost `json:"field_post"`
+	FieldVisibility string    `json:"field_visibility"`
+}
+
+type Relationships struct {
+	FieldRecipientGroup FieldRecipientGroup `json:"field_recipient_group"`
+}
+
+type FieldPost struct {
+	Value  string `json:"value"`
+	Format string `json:"format"`
+}
+
+type FieldRecipientGroup struct {
+	Data RData `json:"data"`
+}
+
+type RData struct {
+	Type string `json:"type"`
+	Id   string `json:"id"`
+}
+
+const (
+	GroupSudbury = "b55fe232-0fbf-4fa8-b697-ff7bb863ae6a"
+)
+
 func main() {
 	if godotenv.Load(".env") != nil {
 		log.Fatal("error loading .env file")
@@ -88,9 +125,37 @@ func processHref(href string) error {
 
 	// Finally, no data means we can publish to Streetcode
 	if len(data.Data) == 0 {
+		// Open our jsonFile
+		jsonFile, err := os.Open("api/post.json")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer jsonFile.Close()
+
+		// read our opened jsonFile as a byte array.
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+
+		// we initialize our PostPhoto array
+		postData := PostPhoto{}
+
+		// we unmarshal our byteArray which contains our
+		// jsonFile's content into 'postData' which we defined above
+		json.Unmarshal(byteValue, &postData)
+
+		postData.Data.Attributes.FieldPost.Value = href
+		postData.Data.Relationships.FieldRecipientGroup.Data.Id = GroupSudbury
+
+		jsonData, err := json.Marshal(postData)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
 		// POST to Streetcode
-		jsonData := fmt.Sprintf(`{"data":{"type":"post--photo","attributes":{"field_post":{"value":"%s","format":"basic_html"},"field_visibility": "1"},"relationships":{"field_recipient_group":{"data":{"type":"group--public_group","id":"b55fe232-0fbf-4fa8-b697-ff7bb863ae6a"}}}}}`, href)
-		request, _ := http.NewRequest("POST", os.Getenv("API_URL"), bytes.NewBuffer([]byte(jsonData)))
+		request, _ := http.NewRequest(
+			"POST",
+			os.Getenv("API_URL"),
+			bytes.NewBuffer([]byte(jsonData)),
+		)
 		request.Header.Set("Content-Type", "application/vnd.api+json")
 		request.Header.Set("Accept", "application/vnd.api+json")
 		request.SetBasicAuth(os.Getenv("USERNAME"), os.Getenv("PASSWORD"))
