7dd4b1b1bad056ae4ea6ff56bbada23c3c8f4d44
Russell Jones <jonesrussell42@gmail.com>
2023-10-09 03:43:06 +0000

    Update consumer for rediswrapper changes

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,66 +1,109 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/joho/godotenv"
-	"github.com/jonesrussell/crawler/internal/post"
-	"github.com/jonesrussell/crawler/internal/rediswrapper"
+	"github.com/go-redis/redis/v8"
+	"github.com/gocolly/colly"
+	"go.uber.org/zap"
 )
 
-func main() {
-	if godotenv.Load(".env") != nil {
-		log.Fatal("error loading .env file")
-	}
+// Consumer represents a consumer that processes URLs from a Redis stream.
+type Consumer struct {
+	RedisClient *redis.Client
+	Collector   *colly.Collector
+	Logger      *zap.SugaredLogger
+	Stream      string
+	Group       string
+}
 
-	// Setup the Redis connection
-	addr := fmt.Sprintf(
-		"%s:%s",
-		os.Getenv("REDIS_HOST"),
-		os.Getenv("REDIS_PORT"),
-	)
-	redisClient := rediswrapper.Connect(addr, os.Getenv("REDIS_AUTH"))
-	defer redisClient.Close()
-
-	// Connect to Redis Stream
-	err := rediswrapper.Stream(os.Getenv("REDIS_STREAM"), os.Getenv("REDIS_GROUP"))
-	if err != nil {
-		log.Println(err)
-	}
-
-	log.Println("consumer started")
-
-	for {
-		entries, err := rediswrapper.Entries(
-			os.Getenv("REDIS_GROUP"),
-			os.Getenv("REDIS_STREAM"),
-		)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		messages := rediswrapper.Messages(entries)
-
-		urls := rediswrapper.Process(
-			messages,
-			os.Getenv("REDIS_STREAM"),
-			os.Getenv("REDIS_GROUP"),
-		)
-
-		consume(urls)
+// NewConsumer creates a new Consumer instance.
+func NewConsumer(redisClient *redis.Client, collector *colly.Collector, logger *zap.SugaredLogger, stream, group string) *Consumer {
+	return &Consumer{
+		RedisClient: redisClient,
+		Collector:   collector,
+		Logger:      logger,
+		Stream:      stream,
+		Group:       group,
 	}
 }
 
-func consume(urls []rediswrapper.MsgPost) {
-	for i := 0; i < len(urls); i++ {
-		href := urls[i]
-		post.SetUsername(os.Getenv("USERNAME"))
-		post.SetPassword(os.Getenv("PASSWORD"))
-		err := post.Process(href, os.Getenv("API_FILTER_URL"))
-		if err != nil {
-			log.Fatal(err)
+// Consume starts listening to the Redis stream and processes URLs.
+func (c *Consumer) Consume(ctx context.Context) error {
+	// Create a consumer group if it doesn't exist
+	if err := c.RedisClient.XGroupCreateMkStream(ctx, c.Stream, c.Group, "$").Err(); err != nil && err != redis.Nil {
+		return err
+	}
+
+	// Create a channel for receiving signals to gracefully stop the consumer
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil // The context is canceled, so we should stop the consumer gracefully.
+		default:
+			// Read messages from the Redis stream
+			messages, err := c.RedisClient.XReadGroup(ctx, &redis.XReadGroupArgs{
+				Group:    c.Group,
+				Consumer: "crawler", // Your consumer name
+				Streams:  []string{c.Stream, ">"},
+				Block:    0,
+			}).Result()
+
+			if err != nil {
+				c.Logger.Errorw("Error reading from Redis stream", "error", err)
+				continue
+			}
+
+			// Process the messages
+			for _, message := range messages {
+				for _, xMessage := range message.Messages {
+					href := xMessage.Values["href"].(string)
+					c.Logger.Infow("Processing URL", "href", href)
+
+					// Add your custom URL processing logic here using the Collector.
+					// For example, you can use c.Collector.Visit(href) to crawl the URL.
+
+					// Acknowledge the message to remove it from the stream
+					if err := c.RedisClient.XAck(ctx, c.Stream, c.Group, xMessage.ID).Err(); err != nil {
+						c.Logger.Errorw("Error acknowledging message", "error", err)
+					}
+				}
+			}
+		case <-stopChan:
+			// Received a signal to stop, so we exit the consumer loop
+			return nil
 		}
+	}
+}
+
+func main() {
+	// Initialize the logger, Redis client, and Colly collector here
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	collector := colly.NewCollector()
+
+	// Create a new Consumer
+	consumer := NewConsumer(redisClient, collector, logger.Sugar(), "my_stream", "my_group")
+
+	// Create a context with cancellation support
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Start the consumer
+	if err := consumer.Consume(ctx); err != nil {
+		fmt.Printf("Consumer error: %v\n", err)
+		os.Exit(1)
 	}
 }
