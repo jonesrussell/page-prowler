f0da899e68cd348ac2e76ccb2d63f128eac487d7
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2023-12-20 05:31:08 +0000

    Replace Fiber with Echo

--- cmd/server.go
+++ cmd/server.go
@@ -2,14 +2,15 @@ package cmd
 
 import (
 	"context"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 	"github.com/spf13/cobra"
 )
 
 var serverCmd = &cobra.Command{
 	Use:   "server",
-	Short: "Start the Fiber server",
+	Short: "Start the Echo server",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -17,15 +18,15 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		app := fiber.New()
+		e := echo.New()
 
-		app.Get("/ping", func(c *fiber.Ctx) error {
-			return c.JSON(fiber.Map{
+		e.GET("/ping", func(c echo.Context) error {
+			return c.JSON(http.StatusOK, map[string]string{
 				"message": "pong",
 			})
 		})
 
-		app.Post("/start-crawling", func(c *fiber.Ctx) error {
+		e.POST("/start-crawling", func(c echo.Context) error {
 			var data struct {
 				URL         string
 				SearchTerms string
@@ -33,8 +34,8 @@ to quickly create a Cobra application.`,
 				MaxDepth    int
 				Debug       bool
 			}
-			if err := c.BodyParser(&data); err != nil {
-				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			if err := c.Bind(&data); err != nil {
+				return c.JSON(http.StatusBadRequest, map[string]string{
 					"error": "Cannot parse JSON",
 				})
 			}
@@ -42,24 +43,24 @@ to quickly create a Cobra application.`,
 			ctx := context.Background()
 			crawlerService, err := initializeManager(ctx, data.Debug)
 			if err != nil {
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				return c.JSON(http.StatusInternalServerError, map[string]string{
 					"error": "Failed to initialize Crawl Manager",
 				})
 			}
 
 			err = startCrawling(ctx, data.URL, data.SearchTerms, data.CrawlSiteID, data.MaxDepth, data.Debug, crawlerService)
 			if err != nil {
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				return c.JSON(http.StatusInternalServerError, map[string]string{
 					"error": "Failed to start crawling",
 				})
 			}
 
-			return c.JSON(fiber.Map{
+			return c.JSON(http.StatusOK, map[string]string{
 				"message": "Crawling started",
 			})
 		})
 
-		app.Listen(":3000")
+		e.Start(":3000")
 	},
 }
 
