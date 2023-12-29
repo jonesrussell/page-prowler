78293a823b3b2943d4d67fa647b9ad740bb55458
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2023-12-29 03:04:09 +0000

    Cleanup crawl functions

--- internal/crawler/crawler_test.go
+++ internal/crawler/crawler_test.go
@@ -2,33 +2,12 @@ package crawler
 
 import (
 	"context"
-	"reflect"
 	"testing"
 
 	"github.com/gocolly/colly"
 	"github.com/jonesrussell/page-prowler/internal/logger"
 	"go.uber.org/zap"
 )
-
-func TestConfigureCollector(t *testing.T) {
-	allowedDomains := []string{"test.com"}
-	maxDepth := 2
-
-	collector := ConfigureCollector(allowedDomains, maxDepth)
-
-	if collector == nil {
-		t.Errorf("Expected collector to be not nil")
-		return
-	}
-
-	if !reflect.DeepEqual(collector.AllowedDomains, allowedDomains) {
-		t.Errorf("Expected allowed domains to be %v, but got %v", allowedDomains, collector.AllowedDomains)
-	}
-
-	if collector.MaxDepth != maxDepth {
-		t.Errorf("Expected max depth to be %d, but got %d", maxDepth, collector.MaxDepth)
-	}
-}
 
 func TestHandleHTMLParsing(t *testing.T) {
 	// Create a mock CrawlManager
@@ -40,7 +19,7 @@ func TestHandleHTMLParsing(t *testing.T) {
 	}
 
 	// Call the function with the mock parameters
-	err := cs.handleHTMLParsing(context.Background(), options)
+	err := cs.setupHTMLParsingHandler(context.Background(), options)
 
 	// Check that no error was returned
 	if err != nil {
@@ -58,7 +37,7 @@ func TestHandleErrorEvents(t *testing.T) {
 	collector := colly.NewCollector()
 
 	// Call the function with the mock parameters
-	cs.handleErrorEvents(collector)
+	cs.setupErrorEventHandler(collector)
 
 	// Trigger an error in the collector
 	collector.OnError(func(r *colly.Response, err error) {
@@ -91,31 +70,4 @@ func TestSetupCrawlingLogic(t *testing.T) {
 	}
 
 	// TODO: Add more checks here to verify that the OnHTML, OnError, OnScraped, and OnRequest methods were called correctly
-}
-
-func TestGetHostFromURL(t *testing.T) {
-	// Create a mock Logger
-	zapLogger, _ := zap.NewDevelopment()
-	logger := &logger.ZapLoggerWrapper{Logger: zapLogger.Sugar()}
-
-	// Define test cases
-	testCases := []struct {
-		url      string
-		expected string
-	}{
-		{"http://example.com/path", "example.com"},
-		{"https://www.example.com", "www.example.com"},
-		// add more test cases here
-	}
-
-	// Run test cases
-	for _, tc := range testCases {
-		host, err := GetHostFromURL(tc.url, logger)
-		if err != nil {
-			t.Errorf("Expected no error, but got %v", err)
-		}
-		if host != tc.expected {
-			t.Errorf("Expected host %v, but got %v", tc.expected, host)
-		}
-	}
 }
