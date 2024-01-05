2393d59cbf601b095da248c1d186f0e230503c9f
Russell Jones <jonesrussell42@gmail.com>
2024-01-05 06:24:37 +0000

    Use /v1 group

--- cmd/api.go
+++ cmd/api.go
@@ -19,9 +19,9 @@ var apiCmd = &cobra.Command{
 	Use:   "api",
 	Short: "Start the API server",
 	Long: `The 'api' command starts the API server. This server handles various requests related to matchlinks and crawling jobs.
-		   For example, you can start a new article matching job with the '/matchlinks' endpoint, or retrieve the status of a job with the '/matchlinks/info/{id}' endpoint.
-		   Similarly, you can start a new crawling job with the '/crawling/start' endpoint, or retrieve the status of a crawling job with the '/crawling/info/{id}' endpoint.
-		   The server also includes a '/ping' endpoint for health checks.`,
+		  For example, you can start a new article matching job with the '/matchlinks' endpoint, or retrieve the status of a job with the '/matchlinks/info/{id}' endpoint.
+		  Similarly, you can start a new crawling job with the '/crawling/start' endpoint, or retrieve the status of a crawling job with the '/crawling/info/{id}' endpoint.
+		  The server also includes a '/ping' endpoint for health checks.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		e := echo.New()
 
@@ -36,8 +36,11 @@ var apiCmd = &cobra.Command{
 
 		apiServerInterface := &api.ApiServerInterface{}
 
-		// Register handlers
-		api.RegisterHandlers(e, apiServerInterface)
+		// Create a group with /v1 prefix
+		v1 := e.Group("/v1")
+
+		// Register handlers under /v1
+		api.RegisterHandlers(v1, apiServerInterface)
 
 		if err := e.Start(":3000"); err != nil {
 			log.Fatalf("Error starting echo server: %v", err)
