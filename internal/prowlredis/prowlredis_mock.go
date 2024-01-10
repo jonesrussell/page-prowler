f1285f15c0c0591ed276d6fa77ce98bc0f8e2624
Russell Jones <jonesrussell42@gmail.com>
2024-01-10 03:03:32 +0000

    better redis testing

--- internal/prowlredis/prowlredis_mock.go
+++ internal/prowlredis/prowlredis_mock.go
@@ -3,33 +3,57 @@ package prowlredis
 import "context"
 
 type MockClient struct {
-	ClientInterface
+	pingErr error
+	data    map[string][]string
 }
 
-func NewMockClient() *MockClient {
-	return &MockClient{}
+func NewMockClient() ClientInterface {
+	return &MockClient{
+		data: make(map[string][]string),
+	}
 }
 
 func (m *MockClient) Ping(ctx context.Context) error {
-	return nil
+	return m.pingErr
 }
 
 func (m *MockClient) SAdd(ctx context.Context, key string, members ...interface{}) error {
+	for _, member := range members {
+		m.data[key] = append(m.data[key], member.(string))
+	}
 	return nil
 }
 
 func (m *MockClient) Del(ctx context.Context, keys ...string) error {
+	for _, key := range keys {
+		delete(m.data, key)
+	}
 	return nil
 }
 
 func (m *MockClient) SMembers(ctx context.Context, key string) ([]string, error) {
-	return nil, nil
+	return m.data[key], nil
 }
 
 func (m *MockClient) SIsMember(ctx context.Context, key string, member interface{}) (bool, error) {
+	members, ok := m.data[key]
+	if !ok {
+		return false, nil
+	}
+	memberStr := member.(string)
+	for _, m := range members {
+		if m == memberStr {
+			return true, nil
+		}
+	}
 	return false, nil
 }
 
 func (m *MockClient) Options() *Options {
-	return &Options{}
+	// Return some default options. You might want to make this configurable.
+	return &Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	}
 }
