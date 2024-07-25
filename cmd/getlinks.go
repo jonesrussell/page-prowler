02d71fda4c90c3075070186e3b41e11332ff5a11
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-07-25 17:23:28 +0000

    commands refactor WIP

--- cmd/getlinks.go
+++ cmd/getlinks.go
@@ -9,44 +9,47 @@ import (
 	"github.com/jonesrussell/page-prowler/internal/consumer"
 	"github.com/jonesrussell/page-prowler/internal/crawler"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-var GetLinksCmd = &cobra.Command{
-	Use:   "getlinks",
-	Short: "Get the list of links for a given siteid",
-	RunE: func(cmd *cobra.Command, _ []string) error {
-		if Siteid == "" {
-			return ErrSiteidRequired
-		}
+// NewGetLinksCmd creates a new getlinks command
+func NewGetLinksCmd() *cobra.Command {
+	getLinksCmd := &cobra.Command{
+		Use:   "getlinks",
+		Short: "Get the list of links for a given siteid",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			siteid := viper.GetString("siteid")
+			if siteid == "" {
+				return ErrSiteidRequired
+			}
 
-		manager, ok := cmd.Context().Value(common.CrawlManagerKey).(*crawler.CrawlManager)
-		if !ok || manager == nil {
-			return fmt.Errorf("CrawlManager is not initialized")
-		}
+			manager, ok := cmd.Context().Value(common.CrawlManagerKey).(*crawler.CrawlManager)
+			if !ok || manager == nil {
+				return fmt.Errorf("CrawlManager is not initialized")
+			}
 
-		output, err := printLinks(cmd.Context(), manager, Siteid)
-		if err != nil {
-			log.Printf("Failed to print links: %v\n", err)
-			return err
-		}
+			output, err := printLinks(cmd.Context(), manager, siteid)
+			if err != nil {
+				log.Printf("Failed to print links: %v\n", err)
+				return err
+			}
 
-		jsonOutput, err := consumer.MarshalOutput(output)
-		if err != nil {
-			return err
-		}
+			jsonOutput, err := consumer.MarshalOutput(output)
+			if err != nil {
+				return err
+			}
 
-		err = printJSON(jsonOutput)
-		if err != nil {
-			log.Printf("Failed to print JSON output: %v\n", err)
-			return err
-		}
+			err = printJSON(jsonOutput)
+			if err != nil {
+				log.Printf("Failed to print JSON output: %v\n", err)
+				return err
+			}
 
-		return nil
-	},
-}
+			return nil
+		},
+	}
 
-func init() {
-	resultsCmd.AddCommand(GetLinksCmd)
+	return getLinksCmd
 }
 
 func printJSON(jsonOutput []byte) error {
