7a2cbb0223e9a950d815fe9214e1300e6d61e878
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-07-29 20:12:14 +0000

    WIP

--- internal/crawler/utils.go
+++ internal/crawler/utils.go
@@ -3,6 +3,7 @@ package crawler
 import (
 	"context"
 	"errors"
+	"strings"
 
 	"github.com/gocolly/colly"
 	"github.com/jonesrussell/page-prowler/models"
@@ -25,19 +26,21 @@ func (cm *CrawlManager) createPageData(href string) models.PageData {
 func (cm *CrawlManager) handleMatchingTerms(options *CrawlOptions, currentURL string, pageData models.PageData, matchingTerms []string) error {
 	cm.Logger.Debug("handleMatchingTerms called")
 
-	pageData.UpdatePageData(matchingTerms) // Update the PageData
+	// Calculate the similarity score
+	similarityScore := cm.TermMatcher.CompareTerms(currentURL, strings.Join(matchingTerms, " "))
 
-	err := cm.ProcessMatchingLink(currentURL, pageData, matchingTerms)
-	if err != nil {
-		return err
-	}
+	pageData.UpdatePageData(matchingTerms, similarityScore) // Update the PageData with the similarity score
+
+	// Append the PageData directly to Results.Pages
+	cm.Results.Pages = append(cm.Results.Pages, pageData)
+
 	cm.UpdateStats(options, matchingTerms)
 
 	// Save the result to Redis
 	ctx := context.Background() // Or use a context from your application
 	key := options.CrawlSiteID
 
-	err = cm.DBManager.SaveResultsToRedis(ctx, []models.PageData{pageData}, key)
+	err := cm.DBManager.SaveResultsToRedis(ctx, []models.PageData{pageData}, key)
 
 	if err != nil {
 		cm.Logger.Error("Error saving result to Redis: ", err)
@@ -58,16 +61,6 @@ func (cm *CrawlManager) processLink(e *colly.HTMLElement, href string) error {
 		}
 	}
 
-	return nil
-}
-
-func (cm *CrawlManager) ProcessMatchingLink(href string, pageData models.PageData, matchingTerms []string) error {
-	if href == "" {
-		return errors.New("Missing URL for matching link")
-	}
-
-	pageData.UpdatePageData(matchingTerms)
-	cm.Results.Pages = append(cm.Results.Pages, pageData)
 	return nil
 }
 
